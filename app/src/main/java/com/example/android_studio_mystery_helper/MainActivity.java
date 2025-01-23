b2573ff6a7a63c775f2f89b77546916b524d3e03
Joel Cain <jcain1@stu.jsu.edu>
2025-01-23 14:05:16 -0600

    Basic functionality added.

--- app/src/main/java/com/example/android_studio_mystery_helper/MainActivity.java
+++ app/src/main/java/com/example/android_studio_mystery_helper/MainActivity.java
@@ -5,13 +5,25 @@ import android.os.Bundle;
 import android.view.View;
 import com.example.android_studio_mystery_helper.databinding.ActivityMainBinding;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private String[] stringArray;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        stringArray = getResources().getStringArray(R.array.string_array);
+    }
+
+    public void onClickButton(View view){
+        Random rand = new Random();
+        int arrayIndex = rand.nextInt(stringArray.length);
+        binding.plotFragmentView.setText(stringArray[arrayIndex]);
     }
 }
